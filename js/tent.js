8a0c2c8869b91e6e527cca7ef9228a78b0f7f6ae
Sevario <Flipmil@hotmail.com>
2017-02-02 14:48:16 +0100

    Links added, dingen gefixed

--- js/tent.js
+++ js/tent.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
 
+    $('.matenform').show();
+    $('.aantal').hide();
+
+    $('#maten').change(function () {
+        var selected = $('#maten option:selected').text();
+        if (selected !== "") {
+            $('.aantal').show('fast');
+        }
+        else {
+            $('.aantal').hide('fast');
+        }
+        
+    
+    });
+    
+    
+    //Show advies input if box checked
+    $('.advies').click(function() {
+        $(".advieschecked").toggle(this.checked);
+        
+    });
+    
     $("input:checkbox").on('click', function() {
       // in the handler, 'this' refers to the box clicked on
       var $box = $(this);
@@ -15,7 +37,26 @@ $(document).ready(function() {
         $box.prop("checked", false);
       }
     });
-
+    //Deselect all options if "geen opties" is selected
+    $('input#geenopties').on('change', function() {
+        $('input#verlichting').not(this).prop('checked', false);  
+        $('input#verwarming').not(this).prop('checked', false);  
+        $('input#systeemvloer').not(this).prop('checked', false);  
+        
+    });
+    
+        //Deselect all other options if "geen deur" is selected
+    $('input#geendeur').on('change', function() {
+        $('input#roldeur').not(this).prop('checked', false);  
+        $('input#overhead').not(this).prop('checked', false);  
+        $('input#enkeledeur').not(this).prop('checked', false);  
+        $('input#dubbeledeur').not(this).prop('checked', false);  
+    });
+    
+    //Deselect geen opties if other options are selected
+    $('input#verlichting').on('change', function() { $('input#geenopties').not(this).prop('checked', false);  });
+    $('input#verwarming').on('change', function() { $('input#geenopties').not(this).prop('checked', false);  });
+    $('input#systeemvloer').on('change', function() { $('input#geenopties').not(this).prop('checked', false);  });
 
 });
 function showForm(){
