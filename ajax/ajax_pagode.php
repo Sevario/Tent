82c7b9ff544e61465119524c78b3c0496b37088d
Sevario <Flipmil@hotmail.com>
2017-02-13 14:16:37 +0100

    Pagode

--- ajax/ajax_pagode.php
+++ ajax/ajax_pagode.php
@@ -12,29 +12,29 @@
             <div class="matenform">
                 <label>1. Maten</label>
                     <br />
-                    <select id='maten'>
+                    <select id='maten' name="pagode_maten">
                         <option value="null"></option>
-                        <option value="option1">4x4</option>
-                        <option value="option2">5x5</option>
+                        <option value="4x4">4x4</option>
+                        <option value="5x5">5x5</option>
                     </select>
                 </div>
             <div class="aantaldiv">
-                 <input class="aantal" type="text" class="form-control" name="aantal" placeholder="Aantal">
+                 <input class="aantal" type="text" class="form-control" name="pagode_aantal" placeholder="Aantal">
             </div>
             <div class="checkhide">
                 <label>2. Vloer</label><br>
                 <label>
-                    <input class="pagode_vloer" type="checkbox" id="pagode_vlonders"  name="pagode_vloer"/>
+                    <input class="pagode_vloer" type="checkbox" id="pagode_vlonders"  name="pagode_vloer" value="Vlonders"/>
                     <label class="checkbox" for="pagode_vlonders"><img src="img/vlonders_leveren.svg" /></label>
                     <p>Vlonders</p>
                 </label>
                 <label>
-                    <input class="pagode_vloer" type="checkbox" id="pagode_systeemvloer" name="pagode_vloer" />
+                    <input class="pagode_vloer" type="checkbox" id="pagode_systeemvloer" name="pagode_vloer" value="Systeem Vloer" />
                     <label class="checkbox" for="pagode_systeemvloer"><img src="img/systeemvloer.svg" /></label>
                     <p>Systeemvloer</p>
                 </label>
                 <label>
-                    <input class="pagode_vloer" type="checkbox" id="pagode_geenvloer" name="pagode_vloer" />
+                    <input class="pagode_vloer" type="checkbox" id="pagode_geenvloer" name="pagode_vloer" value="Geen Vloer"/>
                     <label class="checkbox" for="pagode_geenvloer"><img src="img/geen.svg" /></label>
                     <p>Geen Vloer</p>
                 </label>
@@ -42,17 +42,17 @@
             <div class="checkhide" style="display: none;" id="pagode_toeb">
                 <label>3. Toebehoren</label><br>
                 <label>
-                    <input class="pagode_toeb" type="checkbox" id="pagode_brandblusser" />
+                    <input class="pagode_toeb" type="checkbox" id="pagode_brandblusser" name="pagode_brandblusser" value="Brandblusser"/>
                     <label class="checkbox" for="pagode_brandblusser"><img src="img/brandblusser.svg" /></label>
                     <p>Brandblusser</p>
                 </label>
                 <label>
-                    <input class="pagode_toeb" type="checkbox" id="pagode_noodverlichting" />
+                    <input class="pagode_toeb" type="checkbox" id="pagode_noodverlichting" naam="pagode_noodverlichting" value="Noodverlichting"/>
                     <label class="checkbox" for="pagode_noodverlichting"><img src="img/noodverlichting.svg" /></label>
                     <p>Noodverlichting</p>
                 </label>
                 <label>
-                    <input class="pagode_toeb" type="checkbox" id="pagode_heater" />
+                    <input class="pagode_toeb" type="checkbox" id="pagode_heater" naam=="pagode_heater" value="Heater"/>
                     <label class="checkbox" for="pagode_heater"><img src="img/heater.svg" /></label>
                     <p>Heater</p>
                 </label>
@@ -60,39 +60,39 @@
             <div class="checkhide" style="display: none;" id="pagode_grond">
                 <label>4. Ondergrond</label><br>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_stenenstraat" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_stenenstraat" name="pagode_vloeren" value="Stenen Straat"/>
                     <label class="checkbox" for="pagode_stenenstraat"><img src="img/tegels.svg" /></label>
                     <p>Stenen Straat</p>
                 </label>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_asfalt" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_asfalt" name="pagode_vloeren" value="Asfalt"/>
                     <label class="checkbox" for="pagode_asfalt"><img src="img/asfalt.svg" /></label>
                     <p>Asfalt</p>
                 </label>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_gras" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_gras" name="pagode_vloeren" value="Gras"/>
                     <label class="checkbox" for="pagode_gras"><img src="img/gras.svg" /></label>
                     <p>Gras</p>
                 </label>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_grind" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_grind" name="pagode_vloeren" value="Grind"/>
                     <label class="checkbox" for="pagode_grind"><img src="img/grind.svg" /></label>
                     <p>Grind</p>
                 </label>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_beton" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_beton" name="pagode_vloeren" value="Beton"/>
                     <label class="checkbox" for="pagode_beton"><img src="img/beton.svg" /></label>
                     <p>Beton</p>
                 </label>
                 <label>
-                    <input class="pagode_grond" type="checkbox" id="pagode_strand" name="pagode_vloeren"/>
+                    <input class="pagode_grond" type="checkbox" id="pagode_strand" name="pagode_vloeren" value="Strand"/>
                     <label class="checkbox" for="pagode_strand"><img src="img/strand.svg" /></label>
                     <p>Strand</p>
                 </label> <br>
             </div>
             <div style="display: none;" id="pagode_haringen">
                 <label>
-                     <input class="geenharingen" type="checkbox" name="pagode_haringen" value="geenharingen" /> Er kunnen geen haringen geslagen worden.
+                     <input class="geenharingen" type="checkbox" name="pagode_haringen" value="Er kunnen geen haringen geslagen worden." /> Er kunnen geen haringen geslagen worden.
                 <label>
             </div>
     </body>
