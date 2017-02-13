986ad160b57fa5caa28f1a668745f3b82301d891
Sevario <Flipmil@hotmail.com>
2017-02-13 11:37:21 +0100

    naw

--- naw.php
+++ naw.php
@@ -14,9 +14,9 @@
             <label>Bedrijf of particulier</label>
             <br />
             <select id = "opts" onchange = "showForm()">
-                <option value = "0"></option>
-                <option value = "1">Bedrijf</option>
-                <option value = "2">Particulier</option>
+                <option value = "0" name="nawdrop"></option>
+                <option value = "1" name="nawdrop">Bedrijf</option>
+                <option value = "2" name="nawdrop">Particulier</option>
             </select>
         </div>
         <div id = "f1" style="display: none"><br>
@@ -35,7 +35,7 @@
             <label>Tel.nr:</label>
             <input type="text" name="Tel.nr"><br>
             <label>Tel.nr mobiel:</label>
-            <input type="text" name="Tel.nr mobiel"><br>
+            <input type="text" name="Mobiel.nr"><br>
             <label>Email:</label>
             <input type="text" name="Email"><br>
         </div>
@@ -53,18 +53,18 @@
             <label>Tel.nr:</label>
             <input type="text" name="Tel.nr"><br>
             <label>Tel.nr mobiel:</label>
-            <input type="text" name="Tel.nr mobiel"><br>
+            <input type="text" name="Mobiel.nr"><br>
             <label>Email:</label>
             <input type="text" name="Email"><br>
             <label>Afleveradres</label><br>
-            <input type="checkbox" name="check1">
+            <input type="checkbox" name="check1" value="1">
             <label>Afwijkend van bovenstaand</label><br>
             <label>Adres:</label>
-            <input type="text" name="Adres"><br>
+            <input type="text" name="Adres2"><br>
             <label>Plaats:</label>
-            <input type="text" name="Plaats"><br>
+            <input type="text" name="Plaats2"><br>
             <label>Land:</label>
-            <input type="text" name="Land"><br>
+            <input type="text" name="Land2"><br>
         </div>
     </form>
 </body>
