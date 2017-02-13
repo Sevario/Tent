4b4b96d66d11cdf8698eac9e7a4f0b4792c4f150
Sevario <Flipmil@hotmail.com>
2017-02-13 14:37:15 +0100

    Spantent

--- ajax/ajax_spantent.php
+++ ajax/ajax_spantent.php
@@ -12,37 +12,38 @@
             <div class="breedte">
                 <label>1. Breedte (in meters)</label>
 
-                <select id="breedte">
-                    <option value = "0"></option>
-                    <option value = "1">10</option>
-                    <option value = "2">15</option>
-                    <option value = "3">20</option>
+                <select id="breedte" name="span_breedte">
+                    <option value = ""></option>
+                    <option value = "10">10</option>
+                    <option value = "15">15</option>
+                    <option value = "20">20</option>
+                    <option value = "25">25</option>
                 </select>
                 <label> Lengte (in meters)</label>
-                <select id="lengte">
-                    <option value = "0"></option>
-                    <option value = "1">10</option>
-                    <option value = "2">15</option>
-                    <option value = "3">20</option>
-                    <option value = "1">25</option>
-                    <option value = "2">30</option>
-                    <option value = "3">35</option>
-                    <option value = "1">40</option>
-                    <option value = "2">45</option>
-                    <option value = "3">50</option>
-                    <option value = "1">55</option>
-                    <option value = "2">60</option>
+                <select id="lengte" name="lengte">
+                    <option value = ""></option>
+                    <option value = "10">10</option>
+                    <option value = "15">15</option>
+                    <option value = "20">20</option>
+                    <option value = "25">25</option>
+                    <option value = "30">30</option>
+                    <option value = "35">35</option>
+                    <option value = "40">40</option>
+                    <option value = "45">45</option>
+                    <option value = "50"</option>
+                    <option value = "55">55</option>
+                    <option value = "60">60</option>
                 </select>
             </div>
             <div class="checkhide">
                 <label>2. Vloer</label><br>
                 <label>
-                    <input class="span_vloer" type="checkbox" id="span_vlonders"  name="span_vloer"/>
+                    <input class="span_vloer" type="checkbox" id="span_vlonders"  name="span_vloer" value="Ja"/>
                     <label class="checkbox" for="span_vlonders"><img src="img/vlonders_leveren.svg" /></label>
                     <p>Vlonders</p>
                 </label>
                 <label>
-                    <input class="span_vloer" type="checkbox" id="span_geenvloer" name="span_vloer" />
+                    <input class="span_vloer" type="checkbox" id="span_geenvloer" name="span_vloer" value="Nee"/>
                     <label class="checkbox" for="span_geenvloer"><img src="img/geen.svg" /></label>
                     <p>Geen Vloer</p>
                 </label>
@@ -50,22 +51,22 @@
             <div class="checkhide" style="display:none;" id="span_toeb">
                 <label>3. Toebehoren</label><br>
                 <label>
-                    <input class="span_toeb" type="checkbox" id="span_brandblusser" />
+                    <input class="span_toeb" type="checkbox" id="span_brandblusser" name="span_brandblusser" value="Brandblusser" />
                     <label class="checkbox" for="span_brandblusser"><img src="img/brandblusser.svg" /></label>
                     <p>Brandblusser</p>
                 </label>
                 <label>
-                    <input class="span_toeb" type="checkbox" id="span_noodverlichting" />
+                    <input class="span_toeb" type="checkbox" id="span_noodverlichting" name="span_noodverlichting" value="Noodverlichting"/>
                     <label class="checkbox" for="span_noodverlichting"><img src="img/noodverlichting.svg" /></label>
                     <p>Noodverlichting</p>
                 </label>
                 <label>
-                    <input class="span_toeb" type="checkbox" id="span_heater" />
+                    <input class="span_toeb" type="checkbox" id="span_heater" name="span_heater" value="Heater"/>
                     <label class="checkbox" for="span_heater"><img src="img/heater.svg" /></label>
                     <p>Heater</p>
                 </label>
                 <label>
-                    <input class="span_toeb" type="checkbox" id="span_geen" />
+                    <input class="span_toeb" type="checkbox" id="span_geen" name="span_geen" value="Geen"/>
                     <label class="checkbox" for="span_geen"><img src="img/geen.svg" /></label>
                     <p>Geen</p>
                 </label>
@@ -73,39 +74,39 @@
             <div class="checkhide" style="display:none;" id="span_grond">
                 <label>4. Ondergrond</label><br>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_stenenstraat" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_stenenstraat" name="span_straat" value="Tegels"/>
                     <label class="checkbox" for="span_stenenstraat"><img src="img/tegels.svg" /></label>
                     <p>Stenen straat</p>
                 </label>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_asfalt" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_asfalt" name="span_straat" value="Asfalt"/>
                     <label class="checkbox" for="span_asfalt"><img src="img/asfalt.svg" /></label>
                     <p>Asfalt</p>
                 </label>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_gras" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_gras" name="span_straat" value="Gras"/>
                     <label class="checkbox" for="span_gras"><img src="img/gras.svg" /></label>
                     <p>Gras</p>
                 </label>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_grind" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_grind" name="span_straat" value="Grind"/>
                     <label class="checkbox" for="span_grind"><img src="img/grind.svg" /></label>
                     <p>Grind</p>
                 </label>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_beton" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_beton" name="span_straat" value="Beton"/>
                     <label class="checkbox" for="beton"><img src="img/beton.svg" /></label>
                     <p>Beton</p>
                 </label>
                 <label>
-                    <input class="span_grond" type="checkbox" id="span_strand" name="span_straat"/>
+                    <input class="span_grond" type="checkbox" id="span_strand" name="span_straat" value="Strand"/>
                     <label class="checkbox" for="span_strand"><img src="img/strand.svg" /></label>
                     <p>Strand</p>
                 </label>
             </div>
         <div style="display:none;" id="span_haringen">
                 <label>
-                    <input class="geenharingen" type="checkbox" name="span_haringen" value="geenharingen">
+                    <input class="geenharingen" type="checkbox" name="span_haringen" value="Er kunnen geen haringen worden geslagen.">
                     Er kunnen geen haringen geslagen worden.<br>
                 </label><br>
             </div>
